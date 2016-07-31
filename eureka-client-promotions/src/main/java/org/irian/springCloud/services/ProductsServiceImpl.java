4cc3e8eee7c0689f9e81ad59010835dfd048e079
Paul Calinovici <calinovicipaul@gmail.com>
2016-07-31 22:16:10 +0300

    Histrix solution

--- eureka-client-promotions/src/main/java/org/irian/springCloud/services/ProductsServiceImpl.java
+++ eureka-client-promotions/src/main/java/org/irian/springCloud/services/ProductsServiceImpl.java
@@ -1,12 +1,15 @@
 package org.irian.springCloud.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.irian.springCloud.feignClients.ElectronicsClient;
 import org.irian.springCloud.feignClients.FoodClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by IrianLaptop on 7/31/2016.
  */
+@Service
 public class ProductsServiceImpl implements ProductsService {
 
     @Autowired
@@ -16,12 +19,22 @@ public class ProductsServiceImpl implements ProductsService {
 
 
     @Override
+    @HystrixCommand(fallbackMethod = "getFallbackElectronicsProduct")
     public String getElectronicsProduct() {
         return electronicsClient.getProduct();
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "getFallbackFoodProduct")
     public String getFoodProduct() {
         return foodClient.getProduct();
+    }
+
+    public String getFallbackElectronicsProduct(){
+        return "coffee-maker";
+    }
+
+    public String getFallbackFoodProduct(){
+        return "Yoghurt";
     }
 }
